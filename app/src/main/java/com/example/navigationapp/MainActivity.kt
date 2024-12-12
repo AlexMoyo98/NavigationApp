08dc93932b99aa6422f9e8517b12d7d27074364e
Alex Moyo <76572132+AlexMoyo98@users.noreply.github.com>
2024-12-13 01:58:45 +0200

    navigation app commit

---
+++ app/src/main/java/com/example/navigationapp/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.navigationapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.navigationapp.ui.theme.NavigationAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "firstscreen") {
+        composable("firstscreen") {
+            FirstScreen { name, age ->
+                navController.navigate("secondscreen/$name/$age")
+            }
+        }
+        composable(route = "secondscreen/{name}/{age}",
+            arguments = listOf(
+                navArgument("name") { type = NavType.StringType },
+                navArgument("age") { type = NavType.IntType }
+            )) { backStackEntry ->
+            val name = backStackEntry.arguments?.getString("name") ?: "no name"
+            val age = backStackEntry.arguments?.getInt("age") ?: 0
+            SecondScreen(name,age) {
+                navController.navigate("firstscreen")
+            }
+        }
+    }
+}
